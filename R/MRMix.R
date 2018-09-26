0fb0f039b3631de05bb1c26ab4f360609f3eff33
gqi <guanghaoqi@gmail.com>
2018-09-26 12:08:05 -0400

    Revised documentation.

--- R/MRMix.R
+++ R/MRMix.R
@@ -1,15 +1,15 @@
-#' Mendelian randomization analysis using mixture models
+#' Two-sample Mendelian randomization analysis using mixture models
 #'
-#' @description Conducts Mendelian randomization analysis by fitting a mixture normal model for the bivariate effect size distribution of the exposure and the outcome.
+#' @description Conducts Mendelian randomization analysis by fitting a mixture normal model for the bivariate effect size distribution of the exposure and the outcome. Uses GWAS summary level data to identify causal effects.
 #'
 #' @param betahat_x GWAS effect estimates of the exposure. Vector of length \code{K}, where \code{K} is the number of instruments.
 #' @param betahat_y GWAS effect estimates of the outcome. Vector of length \code{K}.
 #' @param sx2 Variance of \code{betahat_x}. Vector of length \code{K}.
 #' @param sy2 Variance of \code{betahat_y}. Vector of length \code{K}.
-#' @param theta_temp_vec A vector of the grid search values of the causal effect \code{theta}.
+#' @param theta_temp_vec A vector of the grid search values for the causal effect \code{theta}.
 #' @param pi_init Initial value of the probability mass at the null component. See details.
 #' @param sigma_init Initial value of the variance of the non-null component. See details.
-#' @param profile Logical. Default to be \code{FALSE} and only a list of 3 containing the estimate of causal effect (\code{theta}) and corresponding \code{pi0} and \code{sigma2}. If TRUE, returns a matrix of 3 columns. The first column is \code{theta_temp_vec}. The second and third column are the corresponding \code{pi0} and \code{sigma2} values.
+#' @param profile Logical. Default to be \code{FALSE} and only return a list of 3 containing the estimate of causal effect (\code{theta}) and corresponding \code{pi0} and \code{sigma2}. If TRUE, returns a matrix of 3 columns. The first column is \code{theta_temp_vec}. The second and third columns are the corresponding \code{pi0} and \code{sigma2} values.
 
 #' @details The algorithm searches over a grid of possible values of the causal effect \code{theta}. For each fixed \code{theta}, fit mixture model \code{pi0*N(0,sy2+theta^2 sx2)+(1-pi0)*N(0,sigma2)} on the residual \code{betahat_y-theta*betahat_x}. Choose the value of \code{theta} that have the maximum \code{pi0} as the estimate of causal effect.
 #'
@@ -49,10 +49,6 @@ MRMix = function(betahat_x, betahat_y, sx2, sy2, theta_temp_vec = seq(-0.49,0.5,
             pt[pt>0.9999999] = 0.9999999
             pi0 = mean(pt)
             sigma0 = sum((1-pt)*(betahat_y-theta_temp*betahat_x)^2)/(length(betahat_x)-sum(pt))
-
-            # if (pi0<1e-10) pi0 = 1e-10
-            # if (pi0>0.9999999) pi0 = 0.9999999
-            # if (sigma0>0.01) sigma0=0.01
 
             if (pi0<0.0001) pi0 = 0.0001
             if (pi0>0.9999) pi0 = 0.9999
